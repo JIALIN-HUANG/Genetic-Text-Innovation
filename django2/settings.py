65c9c7b28c3154ec966de88e2122ac84733687b7
Yuexin Chen <yuexinc1@uci.edu>
2023-10-05 19:34:30 -0700

    Revert "Updated settings.py to allow vercel.app to be a valid hostname. Updated vercel.json with custom size limit of bundle and specific python runtime version."
    This reverts commit 2b3d6028dee8369fef402b916a1701a824f09418.

--- django2/settings.py
+++ django2/settings.py
@@ -27,7 +27,7 @@ ENV = os.environ.get('environment', 'dev').lower()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if ENV == 'dev' else False
 
-ALLOWED_HOSTS = ['*', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -79,7 +79,12 @@ WSGI_APPLICATION = 'django2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {} # Prevent Django from loading an adapter
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
